07016a0d64f9d2541ae511d087f438375a12fadf
SaGaGro <railp1594@gmail.com>
2024-10-24 22:38:40 +0700

    LoginPage

--- customer/App.js
+++ customer/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import TabNavigator from './components/TabNavigator';
+import Loginpage from './pages/LoginPage/Loginpage';
 
 
   
   const App = () => {
   return (
     <NavigationContainer>
-      <TabNavigator />
+      {/* <TabNavigator /> */}
+      <Loginpage></Loginpage>
     </NavigationContainer>
   );
 
