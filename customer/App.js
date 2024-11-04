c42e4085a4d02f279df04fe22d64ea3fe493930a
GoodNightMare <nonthee.night@gmail.com>
2024-11-04 09:32:11 +0700

    night
    mappage false

--- customer/App.js
+++ customer/App.js
@@ -27,7 +27,7 @@ function HomeStack() {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Mapdetail" component={MapDetail} options={{ headerShown: false }}/>
-      <Stack.Screen name="MapPage" component={MapPage} />
+      <Stack.Screen name="MapPage" component={MapPage} options={{ headerShown: false }} />
       <Stack.Screen name="Order" component={Order}  />
       <Stack.Screen name="payment" component={PaymentPage} options={{ headerShown: false }}/>
       <Stack.Screen name="viewOrder" component={ViewOrder} options={{ headerShown: false }}/>
