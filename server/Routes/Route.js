f895725ba0575e214f4b1a3785ec27c604be770f
Sayhighz <sayhixdd@gmail.com>
2024-11-08 20:35:56 +0700

    Update Route.js

--- server/Routes/Route.js
+++ server/Routes/Route.js
@@ -6,7 +6,6 @@ import path from "path";
 
 const router = express.Router();
 
-// //end upload
 
 router.post("/add_request", (req, res) => {
     const sql = `
@@ -44,6 +43,33 @@ router.post("/add_request", (req, res) => {
       return res.json({ Status: true, InsertId: result.insertId });
     });
   });
+
+  router.get("/service_history_customer", (req, res) => {
+    const sql = `
+      SELECT 
+    sr.vehicle_type,
+    sr.request_time AS date,
+    sr.status AS service_status,
+    sr.location_from AS origin,
+    sr.location_to AS destination,
+    do.offered_price AS service_charge
+    FROM 
+        ServiceRequests sr
+    LEFT JOIN 
+        DriverOffers do ON sr.request_id = do.request_id AND do.offer_status = 'accepted'
+    WHERE 
+        sr.customer_id = 1  -- Replace '?' with the actual customer_id or use it as a placeholder for prepared statements
+    ORDER BY 
+        sr.request_time DESC;
+
+    `;
+  
+    con.query(sql, values, (err, result) => {
+      if (err) return res.json({ Status: false, Error: err.message });
+      return res.json({ Status: true, InsertId: result.insertId });
+    });
+  });
+
 
 // // req orderNo
 export { router as adminRouter };
