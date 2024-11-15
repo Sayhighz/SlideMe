c4d7c8c911df4dfabc1942aed6e414dc9c3819c6
Pratan Nilson <119069194+Sayhighz@users.noreply.github.com>
2024-11-15 17:15:00 +0700

    Add Api
    - Add Reviews

--- server/Routes/Route.js
+++ server/Routes/Route.js
@@ -111,5 +111,31 @@ const values = [
   });
 
 
+  router.post("/add_reviews", (req, res) => {
+    const sql = `
+    INSERT INTO reviews (
+      request_id,
+      customer_id,
+      driver_id,
+      rating,
+      review_text
+  ) VALUES (?, ?, ?, ?, ?)
+  `;
+  
+  const values = [
+    req.body.request_id,
+    req.body.customer_id,
+    req.body.driver_id,
+    req.body.rating,
+    req.body.review_text
+  ];
+    
+      con.query(sql, values, (err, result) => {
+        if (err) return res.json({ Status: false, Error: err.message });
+        return res.json({ Status: true, InsertId: result.insertId });
+      });
+    });
+
+
 
 export { router as adminRouter };
